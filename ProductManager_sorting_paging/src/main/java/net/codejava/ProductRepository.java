16e5cd110472041fe256e0150e4a7e562c30c834
yiorgosdi <ydimitriades@gmail.com>
2020-05-18 11:44:41 +0300

    project created

---
+++ ProductManager_sorting_paging/src/main/java/net/codejava/ProductRepository.java
@@ -0,0 +1,10 @@
+package net.codejava;
+
+// import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+// public interface ProductRepository extends JpaRepository<Product, Long> {
+
+   public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+	
+}
